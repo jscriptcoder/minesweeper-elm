a1083388a9fc0338a2f30d648c0c8314cca12922
Francisco Ramos <jscriptcoder@gmail.com>
2016-09-06 12:20:50 +0200

    generate random and unique list of Int

---
+++ showcase/temp4.elm
@@ -0,0 +1,76 @@
+import Html exposing (div, button, text)
+import Html.App exposing (program)
+import Task
+import Time
+import Random
+import List
+import Array
+
+main =
+  program { init = (model, getTime)
+          , view = view
+          , update = update
+          , subscriptions = (\_ -> Sub.none)
+          }
+
+type Msg = Timestamp Float
+
+model = { time = 0
+        , list = []
+        , random = []
+        }
+
+view model =
+  div [][ text <| (toString model.random) ++ " -> " ++ (toString model.list) ]
+
+update msg model =
+  case msg of
+    Timestamp time ->
+    let
+      seed = generateSeed time
+      generatorList = createGenerator 5 1 5
+      randomList = generateRandomList generatorList seed
+      uniqueList = sanitizeList randomList 1 5
+    in
+      ({ model | time = time, list = uniqueList, random = randomList }, Cmd.none)
+
+getTime =
+  Task.perform Timestamp Timestamp Time.now
+
+generateSeed time =
+  Random.initialSeed <| round time
+
+createGenerator size min max =
+  Random.list size <| Random.int min max
+
+generateRandomList generatorList seed =
+  fst <| Random.step generatorList seed
+
+sanitizeList list min max =
+  doSanitationFrom list [] min max
+
+doSanitationFrom list result min max =
+  let
+    maybeHead = List.head list
+    maybeTail = List.tail list
+  in
+    case maybeHead of
+      Just head ->
+      
+        case maybeTail of
+          Just tail ->
+          
+            if List.member head tail || List.member head result then
+              if (head + 1) <= max then
+                doSanitationFrom ((head + 1) :: tail) result min max
+              else
+                doSanitationFrom (min :: tail) result min max
+            else
+              -- doSanitationFrom tail (head :: result) min max
+              doSanitationFrom tail (List.append result [head]) min max
+          
+          Nothing ->
+            result
+          
+      Nothing ->
+        result
